1b3f8481f402a10d81c7b5f09cd37202ff0ef8e3
Gustavo Terrell <Gustavo Terrell>
2017-07-06 13:54:25 -0300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,6 @@
+##How to run the project
+
+1. Add the remote repository: 'git remote add origin https://github.com/GustavoMuleTraining/maven-project-gterrell.git'
+2. Enter the repo: 'cd maven-project'
+3. (Optional) Set your MULE_HOME env variable: 'export MULE_HOME=[locationOfMuleInstall]'
+4. Package and Deploy: 'mvn install' 
---
+++ src/test/java/HelloMavenTest.java
@@ -0,0 +1,20 @@
+import org.mule.tck.junit4.FunctionalTestCase;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class HelloMavenTest extends FunctionalTestCase {
+
+	@Test
+	public void mavenFlowReturnsHelloMaven() throws Exception
+	{
+		runFlowAndExpect("mavenFlow", "Hello Maven!");
+	}
+	
+	@Override
+	protected String getConfigFile(){
+		return "maven-project.xml";
+	}
+	
+}
